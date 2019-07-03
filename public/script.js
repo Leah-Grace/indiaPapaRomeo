94d5e26f3948297b2f838d144801961416a3bc06
Leah-Grace <leahgdg@hotmail.com>
2019-07-02 20:00:26 -0400

    showAll() and /getall route success

--- public/script.js
+++ public/script.js
@@ -1,13 +1,22 @@
 //REFERENCE MERN2 EXPRES
 //show PCA Profile
 function showPCAProfile() {
-  //fix input to _id
-  console.log("let's party");
   const input = document.getElementById("showPCAProfileInput").value;
   console.log(`Show PCA Profile`);
 
-  //show the profile then render it on different page?
+  //show the profile
+  //LATER >>> then render it on different page
   axios.get("/getone/" + input).then(response => {
-    document.getElementById("showPCAProfile").innerHTML = response.data;
+    document.getElementById("showPCAProfile").innerHTML = JSON.stringify(
+      response.data
+    );
+    console.log(typeof response.data);
+    console.log(response.data);
+  });
+}
+
+function showAll() {
+  axios.get("getall").then(res => {
+    document.getElementById("PCAList").innerHTML = JSON.stringify(res.data);
   });
 }
